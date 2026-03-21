bf70336e3247205743f3f1286fab9c5173620542
mariagomez08 <mariiangelgomezsg@gmail.com>
2026-03-20 21:09:05 -0500

    Se agrega avances del ejercicio 1

--- ejercicio1.js
+++ ejercicio1.js
@@ -1,74 +1,130 @@
-// Clase Empleado
-//
+//! Clase empleado
 class Empleado {
-  constructor(nombre, cargo, salario) {
-    this.nombre = nombre;
-    this.cargo = cargo;
-    this.salario = salario;
+    constructor(nombre, cargo, salario) {
+        this.nombre = nombre;
+        this.cargo = cargo;
+        this.salario = salario;
+        this.activo = true;
+    }
+    //! Creación de métodos
+    //? Método para presentarse
+    presentarse() {
+        console.log(`Hola, mi nombre es ${this.nombre} y soy ${this.cargo}`);
+    }
+    //? Método para calcular el salario anual
+    calcularSalarioAnual() {
+        console.log(`Salario anual es ${this.salario * 12}`);
+        return this.salario * 12;
+    }
+    //? Método para desactivar al empleado
+    desactivar() {
+        this.activo = false;
+        console.log(`${this.nombre} ha sido desactivado`);
+        return this.activo;
+    }
+    //? Método para mostrar la información del empleado
+    info() {
+        console.log(`    Información de empleado:
+    Nombre: ${this.nombre}
+    Su cargo actual es: ${this.cargo}
+    Su Sueldo es: ${this.salario}
+    Su estado activdo es: ${this.activo}\n`)
+    }
 
-    this.activo = true;
-  }
-  presentarse() {
-    return `Hola, mi nombre es ${this.nombre} y soy ${this.cargo}`;
-  }
-  calcularSalarioAnual() {
-    let salarioAnual = this.salario * 12;
-    // console.log(`Salario anual es ${salarioAnual}`);
-    return salarioAnual;
-  }
-  desactivar() {
-    this.activo = false;
-    return `Su estado es inactivo `;
-  }
-  info() {
-    return `El empleado es:  ${this.nombre}\n Su cargo actual es: ${this.cargo}\n Su Sueldo es: ${this.salario}\n Su estado es ${this.activo}\n`;
-  }
 }
-let empleado1 = new Empleado("Camilo", "Contador", 200000);
-empleado1.calcularSalarioAnual();
 
-// console.log(empleado1.desactivar() )
-console.log(empleado1.info());
-let empleado2 = new Empleado("Maria", "Auditor", 3500000);
-empleado2.calcularSalarioAnual();
-
-//console.log(empleado2.desactivar() )
-console.log(empleado2.info());
-let empleado3 = new Empleado("Jurany", "Director", 5000000);
-empleado3.calcularSalarioAnual();
-console.log(empleado3.desactivar());
-console.log(empleado3.info());
-
-//Clase extendida: Lider
-//
-
+//! Clase extendida: Lider
 class Lider extends Empleado {
-  constructor(nombre, salario, equipo) {
-    super(nombre, salario);
-    this.equipo = equipo;
-    this.salario = salario;
-    this.cargo = "Lider de equipo";
-  }
-  presentarEquipo() {
-    return this.equipo.join(" ");
-  }
-  agregarMiembro(nombreNuevo) {
-    this.equipo.push(nombreNuevo);
-    return `Bienvenido al equipo ${nombreNuevo}`;
-  }
-  info() {
-    return ` El lider es:  ${this.nombre}\n Su cargo actual es: ${this.cargo}\n Su Sueldo es: ${this.salario}\n Su estado es ${this.activo}\n el número de personas en el equipo son: ${this.equipo.length}`;
-  }
+    constructor(nombre, salario, equipo) {
+        super(nombre, salario)
+        this.nombre = nombre
+        this.salario = salario
+        this.equipo = equipo
+        this.cargo = "Lider de equipo"
+    }
+    //? Método para presentar el equipo
+    presentarEquipo() {
+        console.log(`    Mi equipo está conformado por: ${this.equipo.join(", ")}\n`)
+        return this.equipo.join(" ")
+    }
+    //? Método para agregar un nuevo miembro al equipo
+    agregarMiembro(nombreNuevo) {
+        this.equipo.push(nombreNuevo)
+        console.log(`Bienvenido al equipo, ${nombreNuevo}`)
+    }
+    //? Sobre escribir el método info para mostrar la información del lider
+    info() {
+        console.log(`    El lider es:  ${this.nombre}
+    Su cargo actual es: ${this.cargo}
+    Su Sueldo es: ${this.salario}
+    Su estado es ${this.activo}
+    El número de personas en el equipo son: ${this.equipo.length}`)
+    }
 }
-let lider1 = new Lider("Cesar", 200, ["Camilo", "Maria", "Jurany"]);
-console.log(lider1.calcularSalarioAnual());
-lider1.desactivar();
 
-lider1.agregarMiembro("Marcia");
-console.log(lider1.presentarEquipo());
-console.log(lider1.info());
+//! CHECKPOINT 1:
+//? Creamos 3 instancias de Empleado.
+// let empleado1 = new Empleado("Juan", "Desarrollador", 1000000)
+// let empleado2 = new Empleado("Camilo", "Backend", 2000000)
+// let empleado3 = new Empleado("Quevedo", "Java", 3000000)
 
-let lider2 = new Lider("Juan", 200, ["Camilo", "Maria", "Jurany"]);
-lider2.agregarMiembro("Sandra");
-console.log(lider2.presentarEquipo());
-console.log(lider2.info());
+// empleado1.presentarse();
+// empleado1.calcularSalarioAnual();
+// empleado1.desactivar();
+// empleado1.info();
+
+// empleado2.presentarse();
+// empleado2.calcularSalarioAnual();
+// empleado2.info();
+
+// empleado3.presentarse();
+// empleado3.calcularSalarioAnual();
+// empleado3.desactivar();
+// empleado3.info();
+
+//! CHECKPOINT 2:
+//? Creamos 2 instancias de Lider.
+// let lider1 = new Lider("Juan", 1000000, ["Camilo", "Quev", "Jaim"])
+// let lider2 = new Lider("Yis", 2000000, ["Zam", "San", "Orl"])
+
+// lider1.presentarse()
+// lider1.calcularSalarioAnual()
+// lider1.desactivar()
+// lider1.agregarMiembro("Rocket")
+// lider1.presentarEquipo()
+// lider1.info()
+
+// lider2.presentarse()
+// lider2.calcularSalarioAnual()
+// lider2.agregarMiembro("Dinamita")
+// lider2.presentarEquipo()
+// lider2.info()
+
+//! CHECKPOINT 3:
+//? Crear 4 empleados y 2 lideres.
+let empleado1 = new Empleado("Juan", "Desarrollador", 1000000)
+let empleado2 = new Empleado("Camilo", "Backend", 2000000)
+let empleado3 = new Empleado("Quevedo", "Java", 3000000)
+let empleado4 = new Empleado("Jaim", "Frontend", 4000000)
+
+let lider1 = new Lider("Fulano", 5000000, ["Pepito", "Perez", "Pereira"])
+let lider2 = new Lider("Sultano", 5000000, ["Flor", "Campo", "Sierra"])
+
+//? Calculo de nomina, extrayendo el salario de cada empleado y lider, sumandolos para obtener la nómina mensual.
+let nominaMensual = (empleado1.salario) + (empleado2.salario) + (empleado3.salario) + (empleado4.salario) + (lider1.salario) + (lider2.salario)
+console.log(`La nómina mensual es: ${nominaMensual}`)
+
+//? Calculo de nómina anual, multiplicando la nómina mensual por 12 meses.
+let nominaAnual = nominaMensual * 12
+console.log(`La nómina anual es: ${nominaAnual}`)
+
+//? Desactivar a un empleado y mostrar su información para verificar el cambio de estado.
+empleado1.desactivar()
+empleado1.info()
+
+//? Imprimir Infor() y presentarEquipo() de cada lider para mostrar la información del lider y su equipo.
+lider1.info()
+lider1.presentarEquipo()
+
+lider2.info()
+lider2.presentarEquipo()
